9358e352b544938a66e9c0ef5562af897801a496
Pablo Garcia <pablo.pmza@gmail.com>
2024-12-08 09:04:44 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import MainRoutes from './routes';
+import MainRoutes from './routes.jsx';
 import './styles/global.css'
 
 
